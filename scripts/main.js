60ad17f1abe03ca1c1e86d5569e95d26e7e8c417
danilo <danimoraga1997@gmail.com>
2025-10-07 20:58:53 +0200

    Add more animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 import '../styles/global.css';
 import '../styles/style.css';
-import { animate, stagger } from "animejs";
+import { animate, createTimer, stagger } from "animejs";
 
+// Initial animation
 const container = document.querySelector('.container');
 container.style.left = '-250px';
 
 animate('.container', {
     duration: 1500,
     opacity: 1,
-    translateX: 250
+    translateX: 250,
+    ease: 'outBack'
 });
 
 animate('.container', {
@@ -45,18 +47,37 @@ clickableImage.onclick = () => {
 
 // User picture rotates on click
 const userImage = document.querySelector('.content__bottom__image');
-let aux = true;
+let aux = 0;
 userImage.onclick = () => {
-    if (aux) {
-        animate(userImage, {
-            duration: 1000,
-            rotate: 360
-        })
-    } else {
-        animate(userImage, {
-            duration: 1000,
-            rotate: 0
-        })
-    }
-    aux = !aux;
+    aux = aux == 0 ? 360 : 0;
+    animate(userImage, {
+        duration: 1000,
+        rotate: aux
+    });
+    animate(userImage, {
+        duration: 1000,
+        scale: [1, 1.1, 1]
+    });
+}
+
+// Little diamond icon jumps
+const diamondIcon = document.querySelector('.content__top__data__left__icon');
+diamondIcon.onclick = () => {
+    animate(diamondIcon, {
+        y: [
+            {to: '-1rem', duration: 200, ease: 'outExpo'},
+            {to: 0, duration: 600, ease: 'outBounce'},
+        ]
+    })
+}
+
+// Clock icon rotates
+const clockIcon = document.querySelector('.content__top__data__right__icon');
+let deg = 0;
+clockIcon.onclick = () => {
+    deg += 360;
+    animate(clockIcon, {
+        rotate: deg,
+        duration: 1000
+    });
 }
